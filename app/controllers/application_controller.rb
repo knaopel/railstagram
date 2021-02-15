e574f3215189c1208215abf6cb30afc467ee5304
Kurt Opel <kurt@kurtopel.com>
2021-02-15 11:53:41 -0500

    completed followership

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,20 @@ class ApplicationController < ActionController::Base
 
   layout :layout_by_resource
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   private
 
   def layout_by_resource
     return 'authentication' if devise_controller? && action_name != 'edit'
 
     'application'
+  end
+
+  protected 
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :password, :username, :full_name])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:email, :password, :username])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:email, :password, :username, :full_name])
   end
 end
---
+++ app/controllers/follows_controller.rb
@@ -0,0 +1,27 @@
+class Users::FollowsController < ApplicationController
+  before_action :set_user
+
+  def create
+    if current_user.follow(user.id)
+      respond_to do | format |
+        format.html { redirect_to @user }
+        format.js
+      end
+    end
+  end
+
+  def destroy
+    if current_user.unfollow(@user.id)
+      respond_to do | format |
+        format.html { redirect_to @user }
+        format.js { render action :create }
+      end
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find_by(username: params[:user_username])
+  end
+end
